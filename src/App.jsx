2e9f150a33f8bfed67901e2d21062740c81778a1
Txoba <gerardjovani96@gmail.com>
2023-05-31 00:55:43 +0200

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>
-        helloWorld
-      </p>
-    </>
-  )
+    <div className="App">
+      <Board />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Cell.jsx
@@ -0,0 +1,5 @@
+export default function Cell(props) {
+  let cellName = props.name;
+
+  return <h1>calla</h1>;
+}
